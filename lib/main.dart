e1354433c0ff5515c77014257b467129bcc7b2de
vaibhav0704 <vaibhavsaraf.08@gmail.com>
2022-05-14 15:57:35 +0530

    improved the user verification screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebaseapp/views/login_view.dart';
 import 'package:firebaseapp/views/register_view.dart';
+import 'package:firebaseapp/views/verify_email_view.dart';
 import 'package:flutter/material.dart';
 
 import 'firebase_options.dart';
@@ -33,16 +34,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // print(user);
-            // if (user?.emailVerified ?? false) {
-            // } else {
-            //   Navigator.of(context).push(MaterialPageRoute(
-            //     builder: (context) => const VerifyEmailView(),
-            //   ));
-            // }
-            // return const Text('Done');
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
           default:
             return const CircularProgressIndicator();
         }
