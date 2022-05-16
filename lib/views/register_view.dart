883a7ef16a85011bbbad9024425d33bc0234fd04
vaibhav0704 <vaibhavsaraf.08@gmail.com>
2022-05-16 18:18:22 +0530

    added routing after login

--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import '../firebase_options.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -59,14 +60,14 @@ class _RegisterViewState extends State<RegisterView> {
                     email: email,
                     password: password,
                   );
-                  print(userCredential);
+                  devtools.log(userCredential.toString());
                 } on FirebaseAuthException catch (e) {
                   if (e.code == 'weak-password') {
-                    print('Weak Password');
+                    devtools.log('Weak Password');
                   } else if (e.code == 'email-already-in-use') {
-                    print('Email already in use');
+                    devtools.log('Email already in use');
                   } else if (e.code == 'invalid-email') {
-                    print('Email Invalid');
+                    devtools.log('Email Invalid');
                   }
                 }
                 // FirebaseAuth
